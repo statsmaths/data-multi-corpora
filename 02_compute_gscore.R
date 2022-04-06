275d3b127b4c2dcc3f335d8f419110984aff5839
statsmaths <tbarnold@protonmail.ch>
2022-04-06 09:04:39 -0400

    auto update

--- 02_compute_gscore.R
+++ 02_compute_gscore.R
@@ -2,22 +2,27 @@ library(tidyverse)
 library(stringi)
 
 # helper functions
-read_n_parts <- function(data, fname, n)
+read_n_parts <- function(fname, n)
 {
   output <- vector("list", n)
   for (k in seq_len(n))
   {
-    output[[j]] <- read_csv(sprintf("%s_part%02d.csv.bz2", fname, k))
+    output[[k]] <- read_csv(sprintf("%s_part%02d.csv.bz2", fname, k))
   }
   bind_rows(output)
 }
 
 # which dataset to work with?
-prefix <- "sv"
+prefix <- "fr"
 type <- "news"
 
 # read the data
-docs <- read_csv("output", sprintf("%s_%s_docs.csv.bz2", prefix, type))
-anno <- read_n_parts("output", sprintf("%s_%s_anno", prefix, type), n = 3L)
+docs <- read_csv(sprintf("output/%s_%s_docs.csv.bz2", prefix, type))
+anno <- read_n_parts(sprintf("output/%s_%s_anno", prefix, type), n = 3L)
 
 # run the analysis
+anno %>%
+  filter(lag(token) %in% c("un", "une"), lead(token) %in% "de") %>%
+  group_by(lemma) %>%
+  summarise(n = n()) %>%
+  arrange(desc(n))
